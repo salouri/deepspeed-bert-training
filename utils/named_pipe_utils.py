e20012aa5133e47d1661717b452eca818014eb09
Saeed Badran <saeed.badran@pixelandtheory.com>
2024-08-09 17:54:02 +0300

    add looping to check for readability/writability of pipes before correcsponding operations

--- utils/named_pipe_utils.py
+++ utils/named_pipe_utils.py
@@ -7,18 +7,40 @@ class NamedPipeManager:
         self.create_named_pipe()
 
     def create_named_pipe(self):
+        # Ensure the directory exists
+        os.makedirs(os.path.dirname(self.pipe_path), exist_ok=True)
+        # Create the named pipe if it doesn't exist
         if not os.path.exists(self.pipe_path):
             os.mkfifo(self.pipe_path)
 
     def write_to_pipe(self, data):
-        with open(self.pipe_path, 'w') as pipe:
-            json.dump(data, pipe)
+         rint(f"Reading from pipe: {self.pipe_path}")
+        try:
+            with open(self.pipe_path, 'w') as pipe:
+                while True:
+                    # Use select.select to check if the pipe is writable
+                    _, wlist, _ = select.select([], [pipe], [], 1)
+                    if wlist:
+                        # If writable, write the data to the pipe
+                        json.dump(data, pipe)
+                        pipe.flush()
+                        break
+        except Exception as e:
+            print(f"Error writing to pipe: {e}")
 
     def read_from_pipe(self):
-        with open(self.pipe_path, 'r') as pipe:
-            data = json.load(pipe)
-        return data
-
+        print(f"Reading from pipe: {self.pipe_path}")
+        with open(self.pipe_path, 'r') as pipe:  # Open in read mode
+            while True:
+                rlist, _, _ = select.select([pipe], [], [], 1)  # Wait for data
+                if rlist:
+                    data = pipe.read()
+                    if data:
+                        return data
+                    else:
+                        break
+                else:
+                    print("No data available yet...")
     def remove_named_pipe(self):
         try:
             os.remove(self.pipe_path)
