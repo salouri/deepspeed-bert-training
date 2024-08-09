f38747450bfe8f76c5fe643aa3346b30045b9154
Saeed Badran <saeed.badran@pixelandtheory.com>
2024-08-09 16:24:46 +0300

    rename some methods and remove unused ones

--- utils/named_pipe_utils.py
+++ utils/named_pipe_utils.py
@@ -10,25 +10,14 @@ class NamedPipeManager:
         if not os.path.exists(self.pipe_path):
             os.mkfifo(self.pipe_path)
 
-    def write_data_to_pipe(self, data):
+    def write_to_pipe(self, data):
         with open(self.pipe_path, 'w') as pipe:
             json.dump(data, pipe)
 
-    def read_data_from_pipe(self):
+    def read_from_pipe(self):
         with open(self.pipe_path, 'r') as pipe:
             data = json.load(pipe)
         return data
-
-    @staticmethod
-    def log_metrics_to_pipe(pipe_path, metrics):
-        with open(pipe_path, 'w') as pipe:
-            json.dump(metrics, pipe)
-
-    @staticmethod
-    def read_metrics_from_pipe(pipe_path):
-        with open(pipe_path, 'r') as pipe:
-            metrics = json.load(pipe)
-        return metrics
 
     def remove_named_pipe(self):
         try:
